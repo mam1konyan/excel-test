e060e65bfcaaef5ffe60bb0229e437e78eca5d56
Paruyr Mamikonyan <pashfaf@gmail.com>
2025-02-18 16:03:46 +0400

    fix tailwind background

--- app/_components/label/label.tsx
+++ app/_components/label/label.tsx
@@ -6,9 +6,11 @@ interface Props {
 }
 
 export const Label = ({ children, bulk }: Props) => {
+  const backgroundColor = bulk ? 'bg-transparent' : 'bg-gray-600';
+
   return (
     <div
-      className={`flex w-18 h-6 border-r-1 border-b-1 border-gray-300 flex-shrink-0 justify-center items-center bg-${bulk ? 'transparent' : 'gray-600'}`}
+      className={`flex w-18 h-6 border-r-1 border-b-1 border-gray-300 flex-shrink-0 justify-center items-center ${backgroundColor}`}
     >
       {bulk ? null : children}
     </div>
