d692c5913c5fc163b37c2519cacd134906b3b9de
Paruyr Mamikonyan <pashfaf@gmail.com>
2025-02-18 17:39:29 +0400

    fix cell styles

--- app/_components/cell/cell.tsx
+++ app/_components/cell/cell.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const Cell = ({ style, cellIndex }: Props) => {
   return (
     <input
-      className="w-18 h-6 border-r-1 border-b-1 border-gray-400 text-sm outline-blue-600 focus:ring-0 p-1 m-0 placeholder:font-light placeholder:text-xs placeholder:text-gray-400 placeholder:text-center"
+      className="w-18 h-6 border-r-1 border-b-1 border-gray-400 text-xs text-center outline-blue-600 focus:ring-0 p-1 m-0 placeholder:font-light placeholder:text-xs placeholder:text-gray-400 placeholder:text-center"
       type="number"
       style={style}
       placeholder={cellIndex}
