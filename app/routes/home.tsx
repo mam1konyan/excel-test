ebaa715bfe5b807f0aa64c4db532ce49c5c9245d
Paruyr Mamikonyan <pashfaf@gmail.com>
2025-02-19 09:12:07 +0400

    update description

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Main } from '~/main';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: '10K Excel Sheet' },
-    { name: 'description', content: 'Welcome to 10K Excel Sheet test app!' },
+    { title: '100M Excel Sheet' },
+    { name: 'description', content: 'Welcome to 100M Excel Sheet test app!' },
   ];
 }
 
